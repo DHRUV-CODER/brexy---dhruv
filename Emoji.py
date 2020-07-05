453bc0a89b3d79012431c47a1d0c970aac54f172
DHRUV-CODER <60794694+DHRUV-CODER@users.noreply.github.com>
2020-07-05 20:32:35 +0300

    Create Emoji.py

---
+++ Emoji.py
@@ -0,0 +1,25 @@
+while(True):
+    a  = input("type something:- ")
+
+    if a == "happy":
+        print(a,", 🙂")
+    elif a  == "sad":
+        print(a,", 😭",",I hope your Day's Go Well In this Sad times.")
+    elif a  == "thug life":
+        print(a,", 😎")
+    elif a  == "games":
+        print(", 🎮")
+    elif a  == "victory hand":
+        print(a,", ✌")
+    elif a  == "money":
+        print(a, ",🤑")
+    elif a  == "cool":
+        print(a, ",🆒")
+    elif a  == "lock":
+        print(a, ",🔒")
+    elif a  == "lock and key":
+        print(a, ",🔐")
+    # if the user puts an diiferent input
+    else:
+        print("this emoji will soon come.")
+
