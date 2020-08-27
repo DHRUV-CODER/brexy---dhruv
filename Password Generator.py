79d03294ea18852e0069acb4676925906810b863
Dhruv Nation <60794694+DHRUV-CODER@users.noreply.github.com>
2020-08-27 11:31:04 +0300

    random strong password generator

---
+++ Password Generator.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+if __name__ == '__main__':
+    s1  = string.ascii_lowercase
+    s2 = string.ascii_uppercase
+    s3 = string.digits
+    s4 = string.punctuation
+
+    plen = int(input(("Enter ur password length :- ")))
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+
+    print("ur password is:- ")
+    print("".join(random.sample(s,plen)))
